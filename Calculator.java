21be4a80b611caf81b883aede8e34055f197607b
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-07 15:51:13 +0530

    adding div

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,10 @@ import java.util.*;
 
 
 public class Calculator {
+
+   public static void div(int a,int b){
+  System.out.println(a/b);
+}
     public static void main(String[] args){
         Scanner scn=new Scanner(System.in);
         int a,b,c;
