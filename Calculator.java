e79a83117717dc5ddea7adad00a93cf356a88fac
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-07 15:40:59 +0530

    updated one

--- Calculator.java
+++ Calculator.java
@@ -25,19 +25,19 @@ public class Calculator {
         if(c>0&&c<5){
            if(c==1){
              //System.out.println(a+b);
-             Addition.add(a,b);
+             add(a,b);
            }
            else if(c==2){
              //System.out.println(a-b);
-             Substraction.sub(a,b);
+             sub(a,b);
            }
            else if(c==3){
              //System.out.println(a*b);
-             Multiply.mul(a,b);
+             mul(a,b);
            }
            else if(c==4){
              //System.out.println(a/b);
-             Division.div(a,b);
+             div(a,b);
            }
         }else{
            System.out.println("Invalid Input");
